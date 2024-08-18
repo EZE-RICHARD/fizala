c07791b75e30cd0faa9a7e03b74e38d5200e74c0
Eze Richard <richardeze391@gmail.com>
2024-08-18 12:33:48 +0100

    tri

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+/* import React from "react";
+import { useState } from "react";
 import mainLogo from "/src/assets/FizalaLogo.png";
 import "./styles/sidebar.scss";
 import { NavLink } from "react-router-dom";
@@ -16,7 +17,9 @@ import LogOutBtn from "./LogOutBtn";
 import RoomMateBtn from "./RoomMateBtn";
 
 const SideBar = () => {
-  const classLink = ({ isActive }) => (isActive ? "activeLink" : "");  
+  const classLink = ({ isActive }) => (isActive ? "activeLink text-white" : "");  
+
+  const [border, setborder] = useState()
 
   const navigation = [
     {
@@ -74,16 +77,18 @@ const SideBar = () => {
       <nav className="mt-4">
         <ul className="list-unstyled">
           {navigation.map((item, index) => (
-            <li
+            <NavLink
               key={index}
-              className={classLink({
-                isActive: window.location.pathname === item.path,
-              })}
+              to={item.path}
+              end
+              className={classLink}
+              // className={classLink({
+              //   isActive: window.location.pathname === item.path,
+              // })}
             >
-              <NavLink
-                to={item.path}
-                className="navlink"
-                end
+              <li
+                
+                // className={classLink}
                 style={{
                   borderRight:
                     window.location.pathname === item.path
@@ -93,8 +98,8 @@ const SideBar = () => {
               >
                 <span className="">{item.icon}</span>
                 {item.name}
-              </NavLink>
-            </li>
+              </li>
+            </NavLink>
           ))}
         </ul>
         <section className="footBtns h1">
@@ -109,8 +114,105 @@ const SideBar = () => {
 };
 
 export default SideBar;
-
+ */
 /* The possibilties given to you, depends on the power of God in you and not the power of God 
 
 fellowship intiution and communion
 */
+
+
+import React from "react";
+import { useState } from "react";
+import mainLogo from "/src/assets/FizalaLogo.png";
+import "./styles/sidebar.scss";
+import { NavLink } from "react-router-dom";
+import {
+  BiSolidDashboard,
+  BiSolidStore,
+  BiSolidBoltCircle,
+} from "react-icons/bi";
+import { TfiMenuAlt } from "react-icons/tfi";
+import { FaFolderOpen } from "react-icons/fa6";
+import { DiGoogleAnalytics } from "react-icons/di";
+import { HiUserGroup } from "react-icons/hi2";
+import { CgProfile } from "react-icons/cg";
+import LogOutBtn from "./LogOutBtn";
+import RoomMateBtn from "./RoomMateBtn";
+
+const SideBar = () => {
+  const navigation = [
+    {
+      name: "Dashboard",
+      path: "/dashboard",
+      icon: <BiSolidDashboard />,
+    },
+    {
+      name: "My Listings",
+      path: "mylistings",
+      icon: <TfiMenuAlt />,
+    },
+    {
+      name: "All Listings",
+      path: "/all-listings",
+      icon: <FaFolderOpen />,
+    },
+    {
+      name: "My Stores",
+      path: "/my-stores",
+      icon: <BiSolidStore />,
+    },
+    {
+      name: "Distress Sales 🔥",
+      path: "/distress-sales",
+      icon: <BiSolidBoltCircle />,
+    },
+    {
+      name: "Wallet",
+      path: "/wallet",
+      icon: "🤑",
+    },
+    {
+      name: "Analytics",
+      path: "/analytics",
+      icon: <DiGoogleAnalytics />,
+    },
+    {
+      name: "Affiliate Program",
+      path: "/affiliate-program",
+      icon: <HiUserGroup />,
+    },
+    {
+      name: "Profile",
+      path: "/profile",
+      icon: <CgProfile />,
+    },
+  ];
+
+  return (
+    <aside className="sideBar">
+      <div className="brand">
+        <img src={mainLogo} alt="Main Logo" className="p-4" />
+      </div>
+      <nav className="mt-4">
+        <ul className="list-unstyled">
+          <NavLink >
+            <li></li>
+          </NavLink>
+        </ul>
+        <section className="footBtns h1">
+          <RoomMateBtn />
+          <div className="mt-3">
+            <LogOutBtn />
+          </div>
+        </section>
+      </nav>
+    </aside>
+  );
+};
+
+export default SideBar;
+
+
+
+
+
